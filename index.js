c2a36931a3628b1a6583d7ecc67b31cc6fcb0b5b
Jari Kovalainen <itse@jarikovalainen.com>
2023-03-30 10:47:06 +0000

    Index.js

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const question = "How to send an openai api request";
 
 const GPT4Message = [
   { role: "system", content: "You are a helpful assistant." },
-  { role: "user", content: "Who won the world series in 2020?" },
-  { role: "assistant", content: "The Los Angeles Dodgers won the World Series in 2020." },
+  { role: "user", content: "Who won the world cup in 2022?" },
+  { role: "assistant", content: "Argentina won the World Cup in 2022." },
   { role: "user", content: "Where was it played?" }
 ];
 
